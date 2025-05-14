b88c19a62737269d069cd5aa39df596e4f1f870a
Tuyishimireinnocent <tuyishimireinnocent2002@gmail.com>
2025-05-14 17:43:26 +0200

    loading files

--- server.js
+++ server.js
@@ -1,11 +1,56 @@
+import { error } from 'console';
 import http from 'http';
-const server = http.createServer((req,res) => {
+import fs  from 'fs/promises';
+import url from 'url';
+import path from 'path';
+//get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+console.log(__filename,__dirname);
+//import dotenv from 'dotenv'
+
+const server = http.createServer(async (req,res) => {
     //res.setHeader('Content-Type','text/plain' );
     //res.statusCode = 404;
-    res.writeHead(500,{'Content-Type': 'application/json'})
-    res.end(JSON.stringify({ message: 'Server Error'}));
+    try {
+
+        if(req.method === 'GET'){
+             let filePath;
+
+         if(req.url === '/'){
+    
+       filePath = path.join(__dirname,'public', 'index.html');
+
+    } else if(req.url === '/about'){
+
+    filePath = path.join(__dirname,'public', 'about.html');
+        
+    }else{
+  
+      throw new error('not found');
+    }
+    const data = await fs.readFile(filePath)
+    res.setHeader('Content-Type','text/html');
+    res.write(data);
+    res.end();
+
+
+        } else{
+
+            throw new error('Method not allowed')
+        }
+        
+    } catch (error) {
+    res.writeHead(500,{'Content-Type': 'text/plain'})
+    res.end ('Server Error');
+        
+    }
+    
+
+    
 });
-const port = 8000;
+const port = process.env.PORT;
 server.listen(port,() => {
     console.log(`server running on port ${port}`)
 })
